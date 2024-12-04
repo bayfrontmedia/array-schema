07796baa5272dec174830e946d1aa25f27fe8979
John Robinson <john@bayfrontmedia.com>
2024-12-04 17:28:02 -0500

    v2.1 updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `Fixed` for any bug fixes.
 - `Security` in case of vulnerabilities
 
+## [2.1.0] - 2024.12.04
+
+### Removed
+
+- Removed the `InvalidSchemaException` from `SchemaInterface`.
+- Removed example JSON:API schemas.
+
 ## [2.0.0] - 2023.01.26
 
 ### Added
--- src/SchemaInterface.php
+++ src/SchemaInterface.php
@@ -9,14 +9,9 @@ interface SchemaInterface
      * Returns an array conforming to the desired schema.
      *
      * @param array $array (Input array)
-     * @param array $config (Optional configuration array which can be used to pass options necessary to build the
-     *     desired schema)
-     *
+     * @param array $config (Optional configuration array which can be used to pass options necessary to build the desired schema)
      * @return array
-     *
-     * @throws InvalidSchemaException
      */
-
     public static function create(array $array, array $config = []): array;
 
 }
